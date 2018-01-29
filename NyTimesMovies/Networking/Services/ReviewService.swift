ad5a86117379de8f469e061f35a18268912bb575
pmendozans <pmendoza@nearsoft.com>
2018-01-29 15:35:33 -0700

    Added the service to get all reviews

--- NyTimesMovies/Networking/Services/ReviewService.swift
+++ NyTimesMovies/Networking/Services/ReviewService.swift
@@ -8,22 +8,58 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
+
+enum ResourceType: String {
+    case all = "all"
+    case picks = "picks"
+}
+
+enum ReviewOrder: String {
+    case title = "by-title"
+    case publicationDate = "by-publication-date"
+    case openingDate = "by-opening-date"
+}
 
 struct ReviewService {
     private let apiManager = ApiManager()
+    private let parameterSerializer = ParametersSerializer()
     
-    enum ReviewType: String {
-        case all = "all"
-        case picks = "picks"
+    func getMovieReviews(byResourceType type: ResourceType, order: ReviewOrder, offset: Int, completion: ((JSON) -> Void)?, errorHandler: ((Error) -> Void)?){
+        let parameters = parameterSerializer.getMovieReviewsParameters(offset: 0, order: .publicationDate, resourceType: .all)
+        let url = "\(ApiManager().baseUrl)/svc/movies/v2/reviews/picks.json"
+        makeReviewRequest(url: url, parameters: parameters, completion: { jsonResponse in
+            completion?(jsonResponse)
+        }, errorHandler: { error in
+            errorHandler?(error)
+        })
     }
     
-    private func makeReviewRequest(url: String, parameters: Parameters = [:], completion: ((([String: Any]) -> Void))?, errorHandler: ((NSError) -> Void)?){
+    private func makeReviewRequest(url: String, parameters: Parameters, completion: ((JSON) -> Void)?, errorHandler: ((Error) -> Void)?){
         guard let url = URL(string: url) else{
             return
         }
         Alamofire.request(url, method: .get, parameters: parameters).responseJSON { response in
-            guard let json = response.result.value else {
-                return
+            switch response.result {
+            case.success(let value):
+                print(value)
+                guard let statusCode = response.response?.statusCode else {
+                    return
+                }
+                switch statusCode {
+                case 200:
+                    guard let rawJson = value as? [String: Any] else {
+                        return
+                    }
+                    let json = JSON(rawJson)
+                    completion?(json["results"])
+                default:
+                    print("Error in response")
+                }
+                
+            case .failure(let error):
+                print("Error")
+                errorHandler?(error)
             }
         }
     }
