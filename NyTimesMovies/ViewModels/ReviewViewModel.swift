24282039f6d43884814e5d05ff9c5752c55f1547
pmendozans <pmendoza@nearsoft.com>
2018-01-29 15:35:59 -0700

    Added the view Model to get all reviews

--- NyTimesMovies/ViewModels/ReviewViewModel.swift
+++ NyTimesMovies/ViewModels/ReviewViewModel.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+struct ReviewViewModel {
+    
+    let reviewService = ReviewService()
+    
+    func getMovieReviews(offset: Int, completion: (([MovieReview]) -> Void)?, errorHandler: ((Error) -> Void)?){
+
+        reviewService.getMovieReviews(byResourceType: .all, order: .openingDate, offset: 20, completion: { jsonResponse in
+            var moviewReviews: [MovieReview] = []
+            for jsonReview in jsonResponse.arrayValue {
+                let review = MovieReview(json: jsonReview)
+                moviewReviews.append(review)
+            }
+            completion?(moviewReviews)
+        }, errorHandler: { error in
+            errorHandler?(error)
+        })
+    }
+}
