c338c934e4145cdccead83d194f2716dccb30f1f
pmendozans <pmendoza@nearsoft.com>
2018-01-29 17:51:40 -0700

    Crated the ProfileViewController

---
+++ NyTimesMovies/Controllers/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  NyTimesMovies
+//
+//  Created by Pablo Ruiz on 1/29/18.
+//  Copyright © 2018 nearsoft. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
