4787fd5896aea2e0dcb5c2619be0190fc4eee9c2
pmendozans <pmendoza@nearsoft.com>
2018-01-29 17:50:08 -0700

    Google Login integration

---
+++ NyTimesMovies/Reviews-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Reviews-Bridging-Header.h
+//  NyTimesMovies
+//
+//  Created by Pablo Ruiz on 1/29/18.
+//  Copyright © 2018 nearsoft. All rights reserved.
+//
+
+#ifndef Reviews_Bridging_Header_h
+#define Reviews_Bridging_Header_h
+
+#import <Google/SignIn.h>
+
+#endif /* Reviews_Bridging_Header_h */
