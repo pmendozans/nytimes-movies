99053ed27ccb9a0746fbc38e5edc97f0628dcc88
pmendozans <pmendoza@nearsoft.com>
2018-01-30 09:58:45 -0700

    Added extensions as helpers

---
+++ NyTimesMovies/Common/Extensions/DefaultsKeysExtension.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftyUserDefaultsExtension.swift
+//  NyTimesMovies
+//
+//  Created by Pablo Arturo Ruiz Mendoza on 1/29/18.
+//  Copyright © 2018 nearsoft. All rights reserved.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    static let email = DefaultsKey<String>("email")
+    static let isLoged = DefaultsKey<Bool>("isLoged")
+}
