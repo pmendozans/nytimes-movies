2f86e655bc139c55a271f43f093273060646cb33
pmendozans <pmendoza@nearsoft.com>
2018-01-29 17:59:34 -0700

    Added dummy method to simulate email login

--- NyTimesMovies/Common/LoginManager.swift
+++ NyTimesMovies/Common/LoginManager.swift
@@ -9,16 +9,21 @@
 import Foundation
 import GoogleSignIn
 
-typealias LoginCompletionHandler = (() -> Void )
-typealias LoginErrorHandler = (() -> Void )
+typealias LoginCompletionHandler = (() -> Void )?
+typealias LoginErrorHandler = ((String) -> Void )?
 
 struct LoginManager {
     
-    func login(withEmail email: String, password: String, completion: ()){
-        
+    func login(withEmail email: String, password: String, completion: LoginCompletionHandler, errorHandler: LoginErrorHandler){
+        if email == "test@nearsoft.com" && password == "demo" {
+            completion?()
+        }
+        else {
+            errorHandler?("User not found")
+        }
     }
     
-    func login(withGoogle user: GIDGoogleUser){
+    func login(withGoogle user: GIDGoogleUser, completion: LoginCompletionHandler, errorHandler: LoginErrorHandler){
         
     }
 }
